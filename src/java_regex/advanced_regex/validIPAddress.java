94369de5d50431b599209840cabba6951658b48c
Himaanshii <himanshi2157.be21@chitkara.edu.in>
2025-03-27 10:19:14 +0530

    [Himanshi] add. Validate IP Address

---
+++ src/java_regex/advanced_regex/validIPAddress.java
@@ -0,0 +1,22 @@
+package java_regex.advanced_regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class validIPAddress {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter IP Address");
+        String s= sc.nextLine();
+        String regex ="^(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)$";
+        Pattern p = Pattern.compile(regex); // compile regex
+        Matcher m = p.matcher(s); // creates matcher object
+        if(m.matches()){
+            System.out.println(s+" is Valid IP Address");
+        }else{
+            System.out.println("Invalid");
+        }
+    }
+}
