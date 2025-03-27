725b8d980ad9c853ea9bb5b8ae9f5e413fc9f540
Himaanshii <himanshi2157.be21@chitkara.edu.in>
2025-03-27 23:16:10 +0530

    [Himanshi] add. Testing Temperature Converter

---
+++ src/java_JUnit/src/test/java/org/example/convertTempTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class convertTempTest {
+    @Test
+    public void testCelsiusToFahrenheit() {
+        assertEquals(32.0, convertTemp.convertToFahrenheit((0)), 0.01);
+        assertEquals(98.6, convertTemp.convertToFahrenheit((37)), 0.01);
+        assertEquals(212.0, convertTemp.convertToFahrenheit(100), 0.01);
+    }
+
+    @Test
+    public void testFahrenheitToCelsius() {
+        assertEquals(0.0, convertTemp.convertToCelsius(32), 0.01);
+        assertEquals(37.0, convertTemp.convertToCelsius(98.6), 0.01);
+        assertEquals(100.0, convertTemp.convertToCelsius(212), 0.01);
+    }
+}
