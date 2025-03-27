0726f1542813417f8cf6b14b208915a3900966be
Himaanshii <himanshi2157.be21@chitkara.edu.in>
2025-03-27 15:25:11 +0530

    [Himanshi] add. Testing @BeforeEach and @AfterEach Annotations

---
+++ src/java_JUnit/src/test/java/org/example/DatabaseConnectionTest.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseConnectionTest {
+    private DatabaseConnection dbConnection;
+
+    @BeforeEach
+    void setUp() {
+        dbConnection = new DatabaseConnection();
+        dbConnection.connect();
+    }
+
+    @AfterEach
+    void tearDown() {
+        dbConnection.disconnect();
+    }
+
+    @Test
+    void testConnectionIsEstablished() {
+        assertTrue(dbConnection.isConnected(), "Database should be connected");
+    }
+
+    @Test
+    void testConnectionIsClosed() {
+        dbConnection.disconnect();
+        assertFalse(dbConnection.isConnected(), "Database should be disconnected");
+}
+
+
+}
