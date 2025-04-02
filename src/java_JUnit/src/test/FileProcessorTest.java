626e064262f6b0b8e1d60af5d3f1f7a38e84229c
Himaanshii <himanshi2157.be21@chitkara.edu.in>
2025-04-02 11:40:30 +0530

    [Himanshi] add. Testing File Handling Methods

---
+++ src/java_JUnit/src/test/FileProcessorTest.java
@@ -0,0 +1,39 @@
+package java_junit.src.test;
+
+import java_junit.src.main.FileProcessor;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileProcessorTest {
+    private static final String TEST_FILE = "test.txt";
+    private FileProcessor fileProcessor = new FileProcessor();
+
+    @AfterEach
+    void cleanUp() throws IOException {
+        Files.deleteIfExists(Path.of(TEST_FILE));
+    }
+
+    @Test
+    void testWriteAndRead() throws IOException {
+        fileProcessor.writeToFile(TEST_FILE, "Hello");
+        assertEquals("Hello", fileProcessor.readFromFile(TEST_FILE));
+    }
+
+    @Test
+    void testFileExists() throws IOException {
+        fileProcessor.writeToFile(TEST_FILE, "Test");
+        assertTrue(Files.exists(Path.of(TEST_FILE)));
+    }
+
+    @Test
+    void testReadNonExistentFile() {
+        assertThrows(IOException.class, () -> fileProcessor.readFromFile("nofile.txt"));
+    }
+
+}
