64473f598228e4a45ab445379020d60270053e99
Himaanshii <himanshi2157.be21@chitkara.edu.in>
2025-04-04 09:58:42 +0530

    [Himanshi] add. Create an Annotation to Mark Important Methods

---
+++ src/java_annotations/Basic/ImportantMethods.java
@@ -0,0 +1,44 @@
+package java_annotations.Basic;
+import java.lang.annotation.*;
+import java.lang.reflect.Method;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface ImportantMethod {
+    String level() default "HIGH";
+}
+class MyService {
+
+    @ImportantMethod(level = "CRITICAL")
+    public void processData() {
+        System.out.println("Processing data...");
+    }
+
+    @ImportantMethod
+    public void generateReport() {
+        System.out.println("Generating report...");
+    }
+
+    @ImportantMethod
+    public void normalMethod() {
+        System.out.println("This is a normal method.");
+    }
+}
+public class ImportantMethods {
+    public static void main(String[] args) {
+
+        Class<MyService> obj = MyService.class;
+
+
+        for (Method method : obj.getDeclaredMethods()) {
+
+            if (method.isAnnotationPresent(ImportantMethod.class)) {
+
+                ImportantMethod annotation = method.getAnnotation(ImportantMethod.class);
+
+
+                System.out.println("Method = " + method.getName() + " | Level = " + annotation.level());
+            }
+        }
+    }
+}
